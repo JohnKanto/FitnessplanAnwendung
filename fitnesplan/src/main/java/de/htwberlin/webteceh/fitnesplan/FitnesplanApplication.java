6715d275d8a65cc580d36c03670501743f313503
marcp <marcpwz6@gmail.com>
2023-10-21 23:31:28 +0200

    projekt start

---
+++ fitnesplan/src/main/java/de/htwberlin/webteceh/fitnesplan/FitnesplanApplication.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webteceh.fitnesplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnesplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnesplanApplication.class, args);
+	}
+
+}
---
+++ fitnesplan/src/test/java/de/htwberlin/webteceh/fitnesplan/FitnesplanApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webteceh.fitnesplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnesplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
